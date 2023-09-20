c9c15751b900cbc37963dd76d5419162bd687a50
gabrieljreed <gabrieljreed@gmail.com>
2023-09-20 10:01:27 -0600

    Basic build functionality

--- rigsys/api/api_rig.py
+++ rigsys/api/api_rig.py
@@ -1,6 +1,15 @@
 """Rig API module."""
 
 
+BUILD_PROXIES = "build_proxies"
+BUILD_MODULES = "build_modules"
+BUILD_DEFORMERS = "build_deformers"
+BUILD_UTILITY = "build_utility"
+BUILD_RIG = "build_rig"
+BUILD_EXPORT = "build_export"
+BUILD_FULL = "build_full"
+
+
 class Rig:
     """Rig class."""
 
@@ -10,3 +19,8 @@ class Rig:
         self.deformerModules = {}
         self.utilityModules = {}
         self.exportModules = {}
+
+    def build(self, buildType: str = BUILD_FULL):
+        """Build the rig."""
+        # TODO: We need to figure out how build order and build type will work together.
+        pass

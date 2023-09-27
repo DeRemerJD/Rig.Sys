786b14bacef591fdbe903df4c11676a0d4ddc9f4
Gabriel Reed <gabrieljreed@gmail.com>
2023-09-26 23:12:09 -0600

    Placeholder utility modules

---
+++ rigsys/modules/utility/bindJoints.py
@@ -0,0 +1,17 @@
+"""Build bind joints utility module."""
+
+
+import rigsys.modules.utility.utilityBase as utilityBase
+
+
+class BindJoints(utilityBase.UtilityModuleBase):
+    """Build bind joints utility module."""
+
+    def __init__(self) -> None:
+        """Initialize the module."""
+        super().__init__()
+
+    def run(self) -> None:
+        """Run the module."""
+        # TODO: Implement
+        pass
---
+++ rigsys/modules/utility/importModel.py
@@ -0,0 +1,17 @@
+"""Import model utility module."""
+
+
+import rigsys.modules.utility.utilityBase as utilityBase
+
+
+class ImportModel(utilityBase.UtilityModuleBase):
+    """Import model utility module."""
+
+    def __init__(self) -> None:
+        """Initialize the module."""
+        super().__init__()
+
+    def run(self) -> None:
+        """Run the module."""
+        # TODO: Implement
+        pass

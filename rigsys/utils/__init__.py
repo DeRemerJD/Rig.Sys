8d72893e31fa4b6e602eb0758c016b30af463d20
Gabriel Reed <gabrieljreed@gmail.com>
2023-09-26 23:03:27 -0600

    Add skeleton for utils submodule

---
+++ rigsys/utils/__init__.py
@@ -0,0 +1,7 @@
+"""Developer utilities for rigsys.
+
+The difference between the utils submodule and the lib submodule is that the utils submodule contains utilities that are
+developer-centric, while the lib submodule contains utilities that are Maya-centric. For example, the utils submodule
+contains a function to unload modules, while the lib submodule contains classes and functions for constructing joints,
+proxies, controls, etc.
+"""
---
+++ rigsys/utils/unload.py
@@ -0,0 +1,40 @@
+"""Utility function to unload modules for use in Maya."""
+
+
+import sys
+
+# if you have some packages that you often reload, you can put them here,
+# and they will get reloaded if "packages" attribute is not explicitly stated
+DEFAULT_RELOAD_PACKAGES = ["rigsys"]
+
+
+def unloadPackages(silent=True, packages=None):
+    """Unload all modules in the given packages.
+
+    Args:
+        silent (bool, optional): Whether to print out the modules that are being unloaded. Defaults to True.
+        packages (list, optional): List of packages to unload. Defaults to "rigsys".
+    """
+    if packages is None:
+        packages = DEFAULT_RELOAD_PACKAGES
+
+    # construct reload list
+    reloadList = []
+    for sysModule in sys.modules.keys():
+        for package in packages:
+            if sysModule.startswith(package):
+                reloadList.append(sysModule)
+
+    # unload everything
+    for moduleToReload in reloadList:
+        try:
+            if sys.modules[moduleToReload] is not None:
+                del (sys.modules[moduleToReload])
+                if not silent:
+                    print(f"Unloaded: {moduleToReload}")
+
+        except KeyError:
+            if silent:
+                pass
+            else:
+                print(f"Could not unload: {moduleToReload}")

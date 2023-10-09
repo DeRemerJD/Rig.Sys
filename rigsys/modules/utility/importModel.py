52a6a96c292079636afb62ab8846b85d0830fd32
gabrieljreed <gabrieljreed@gmail.com>
2023-10-09 10:16:31 -0600

    Fix up importModel.py

--- rigsys/modules/utility/importModel.py
+++ rigsys/modules/utility/importModel.py
@@ -1,5 +1,6 @@
 """Import model utility module."""
 
+import os
 
 import rigsys.modules.utility.utilityBase as utilityBase
 
@@ -9,15 +10,19 @@ import maya.cmds as cmds
 class ImportModel(utilityBase.UtilityModuleBase):
     """Import model utility module."""
 
-    def __init__(self, filePath: str, underGroup: str = None) -> None:
+    def __init__(self, rig, name: str = "", buildOrder: int = 3000, isMuted: bool = False, filePath: str = "",
+                 underGroup: str = None) -> None:
         """Initialize the module."""
-        super().__init__()
+        super().__init__(rig, name, buildOrder, isMuted)
 
         self.filePath = filePath
         self.underGroup = underGroup
 
     def run(self) -> None:
         """Run the module."""
+        if not os.path.exists(self.filePath):
+            raise ValueError(f"File does not exist: {self.filePath}")
+
         filePathLowercase = self.filePath.lower()
         if filePathLowercase.endswith(".ma"):
             fileType = "mayaAscii"
@@ -34,18 +39,25 @@ class ImportModel(utilityBase.UtilityModuleBase):
 
         rootNodes = []
         for node in newNodes:
+            if not cmds.objExists(node):
+                print(f"node does not exist: {node}")
+                continue
+
             parent = cmds.listRelatives(node, p=True)
             if not parent:
                 nodeTypes = cmds.nodeType(node, i=True)
                 if "dagNode" in nodeTypes:
                     rootNodes.append(node)
 
-        if self.underGroup is None:
-            return
+        if self.underGroup is None or self.underGroup == "":
+            underGroup = self._rig.rigNode
+        else:
+            underGroup = self.underGroup
 
-        if not cmds.objExists(self.underGroup):
-            cmds.group(n=self.underGroup, em=True)
-            # TODO: This should be parented under the rig node
+            if not cmds.objExists(self.underGroup):
+                underGroup = cmds.group(n=self.underGroup, em=True)
+                cmds.parent(underGroup, self._rig.rigNode)
 
         for node in rootNodes:
-            cmds.parent(node, self.underGroup)
+            print(f"Parenting {node} under {underGroup}")
+            cmds.parent(node, underGroup)

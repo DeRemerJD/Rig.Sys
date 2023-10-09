6975e764e23c4e24b5a945cb4cd989dfc6db62ed
gabrieljreed <gabrieljreed@gmail.com>
2023-10-09 11:07:44 -0600

    Move tests inside rigsys package

--- rigsys/test/modules/utility/test_importModel.py
+++ rigsys/test/modules/utility/test_importModel.py
@@ -3,6 +3,8 @@
 import os
 import unittest
 
+import maya.cmds as cmds
+
 import rigsys.api.api_rig as api_rig
 import rigsys.modules.utility as utility
 
@@ -21,13 +23,30 @@ class TestImportModel(unittest.TestCase):
 
     def setUp(self) -> None:
         """Set up the test."""
+        cmds.file(new=True, force=True)
+
+        self.rig = api_rig.Rig()
+
+        self.resourcesFolder = os.path.join(os.path.dirname(__file__), os.pardir, os.pardir, "resources")
+
         return super().setUp()
 
     def tearDown(self) -> None:
         """Tear down the test."""
         return super().tearDown()
 
-    def test_importModel(self):
+    def test_importFBX(self):
         """Test the ImportModel module."""
-        rig = api_rig.Rig()
-        rig.utilityModules = {"ImportModel": utility.ImportModel(rig, "ImportModel", filePath="test/data/test.ma")}
+        importFilePath = os.path.join(self.resourcesFolder, "cube.fbx")
+
+        self.rig.utilityModules = {
+            "ImportModel": utility.ImportModel(
+                self.rig,
+                "ImportModel",
+                filePath=importFilePath,
+            ),
+        }
+
+        self.rig.build()
+
+        self.assertTrue(cmds.objExists("pCube1"))

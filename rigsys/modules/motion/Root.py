01d1cd56b22ec28485affab76ce9742cff1912a2
gabrieljreed <gabrieljreed@gmail.com>
2023-10-14 12:16:02 -0600

    Formatting

--- rigsys/modules/motion/Root.py
+++ rigsys/modules/motion/Root.py
@@ -16,10 +16,9 @@ class Root(motionBase.MotionModuleBase):
         self.name = "Root"
         self.addOffset = True
 
-
         # Proxy?
         self.proxies = {
-            "Root": rsProxy(blahblahblah, position=[0,0,0], rotation=[0,0,0]),
+            "Root": rsProxy(blahblahblah, position=[0, 0, 0], rotation=[0, 0, 0]),
         }
 
         super().__init__(args, kwargs)
@@ -27,23 +26,25 @@ class Root(motionBase.MotionModuleBase):
     def run(self) -> None:
         """Run the module."""
         # TODO: Implement
-        
+
         # Build overall structure
         proxyPosition = self.proxies["Root"].position
 
         # Structure
-        rootPar = cmds.createNode("transform", n=side+label+self.name+"_grp")
-        rootCtrl = cmds.createNode("transform", n=side+label+self.name+"_CTRL")
+        rootPar = cmds.createNode("transform", n=side + label + self.name + "_grp")
+        rootCtrl = cmds.createNode("transform", n=side + label + self.name + "_CTRL")
         cmds.parent(rootCtrl, rootPar)
         rootCtrlObj = ctrlCrv.Ctrl(node=rootCtrl, shape="circle")
         rootCtrlObj.giveCtrlShape()
         if self.addOffset:
-            offsetPar = cmds.createNode("transform", n=side+label+self.name+"_grp")
-            offsetCtrl = cmds.createNode("transform", n=side+label+self.name+"_CTRL")
+            offsetPar = cmds.createNode(
+                "transform", n=side + label + self.name + "_grp"
+            )
+            offsetCtrl = cmds.createNode(
+                "transform", n=side + label + self.name + "_CTRL"
+            )
             cmds.parent(offsetCtrl, offsetPar)
             cmds.parent(offsetPar, rootCtrl)
             offsetCtrlObj = ctrlCrv.Ctrl(node=offsetCtrl, shape="circle")
             offsetCtrlObj.giveCtrlShape()
         cmds.xform(rootPar, ws=True, t=proxyPosition)
-
-

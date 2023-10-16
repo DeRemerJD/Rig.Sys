2681b4e32a665c9405e8435589f5561ca44d7634
gabrieljreed <gabrieljreed@gmail.com>
2023-10-16 12:40:34 -0600

    proxy sides/mirroring

--- rigsys/lib/proxy.py
+++ rigsys/lib/proxy.py
@@ -1,5 +1,9 @@
 """Proxy class for rigsys modules."""
 
+# TODO: @Jacob - at Zubio we had proxies for nurbs, curves, etc. Do we want any of those here?
+
+import copy
+
 
 class Proxy:
     """Proxy class for rigsys modules."""
@@ -14,3 +18,29 @@ class Proxy:
     def build(self):
         """Build the proxy."""
         pass
+
+    def getFullName(self):
+        """Return the full name of the proxy."""
+        return f"{self.side}_{self.label}"
+
+    def doMirror(self):
+        """Mirror the proxy, returning a new proxy object."""
+        if self.side == "M":
+            print(f"Cannot mirror middle proxy {self.getFullName()}")
+            return None
+
+        newProxy = copy.deepcopy(self)
+
+        if self.side == "L":
+            newProxy.side = "R"
+        elif self.side == "R":
+            newProxy.side = "L"
+
+        # Transform data
+        # TODO: @Jacob, I copied this from the zubio proxies, make sure this is correct
+        newProxy.position[0] *= -1
+        newProxy.rotation[0] *= -1
+        newProxy.rotation[1] *= -1
+        newProxy.rotation[2] *= -1
+
+        return newProxy

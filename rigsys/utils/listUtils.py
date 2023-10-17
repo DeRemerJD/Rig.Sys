3b0741e3ed18dad8533aeb7c26027d57db68170a
gabrieljreed <gabrieljreed@gmail.com>
2023-10-17 16:51:35 -0600

    Improve readability and such

--- rigsys/utils/listUtils.py
+++ rigsys/utils/listUtils.py
@@ -3,12 +3,10 @@
 
 def flattenList(targetList):
     """Flatten a list of lists into a single list."""
-    targetList = list(targetList)
-
-    if targetList == []:
-        return targetList
-
-    if isinstance(targetList[0], list):
-        return flattenList(targetList[0]) + flattenList(targetList[1:])
-
-    return targetList[:1] + flattenList(targetList[1:])
+    newList = []
+    for item in targetList:
+        if isinstance(item, list):
+            newList.extend(item)
+        else:
+            newList.append(item)
+    return newList

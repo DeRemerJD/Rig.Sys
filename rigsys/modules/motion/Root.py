6bc6e4841e9b1b7cc489e6f325bdf5163a56c3e1
gabrieljreed <gabrieljreed@gmail.com>
2023-10-23 14:13:32 -0600

    Formatting fixes

--- rigsys/modules/motion/Root.py
+++ rigsys/modules/motion/Root.py
@@ -11,7 +11,8 @@ import maya.cmds as cmds
 class Root(motionBase.MotionModuleBase):
     """Root Motion Module."""
 
-    def __init__(self, *args, side="", label="", ctrlShapes="circle", ctrlScale=[1.0, 1.0, 1.0], addOffset=True, **kwargs) -> None:
+    def __init__(self, *args, side="", label="", ctrlShapes="circle", ctrlScale=[1.0, 1.0, 1.0], addOffset=True,
+                 **kwargs) -> None:
         """Initialize the module."""
         super().__init__(args, kwargs, side=side, label=label)
         self.addOffset = addOffset
@@ -19,7 +20,13 @@ class Root(motionBase.MotionModuleBase):
         self.ctrlScale = ctrlScale
 
         self.proxies = {
-            "Root": proxy.Proxy(position=[0, 0, 0], rotation=[0, 0, 0], side="M", label="Root", name="Base")
+            "Root": proxy.Proxy(
+                position=[0, 0, 0],
+                rotation=[0, 0, 0],
+                side="M",
+                label="Root",
+                name="Base",
+            )
         }
 
     def buildProxies(self):
@@ -43,19 +50,21 @@ class Root(motionBase.MotionModuleBase):
         self.moduleHierarchy()
 
         # Make Plug Transforms
-        self.plugParent = self.createPlugParent(position=proxyPosition, rotation=proxyRotation)
+        self.plugParent = self.createPlugParent(
+            position=proxyPosition, rotation=proxyRotation
+        )
         self.worldParent = self.createWorldParent()
-
-        
-        
 
         # Structure
         rootPar = cmds.createNode("transform", n=self.getFullName() + "_grp")
         rootCtrl = cmds.createNode("transform", n=self.getFullName() + "_CTRL")
         cmds.parent(rootCtrl, rootPar)
         rootCtrlObj = ctrlCrv.Ctrl(
-            node=rootCtrl, shape=self.ctrlShapes, scale=self.ctrlScale, orient=[90,0,0]
-            )
+            node=rootCtrl,
+            shape=self.ctrlShapes,
+            scale=self.ctrlScale,
+            orient=[90, 0, 0],
+        )
         rootCtrlObj.giveCtrlShape()
         if self.addOffset:
             offsetPar = cmds.createNode(
@@ -68,10 +77,13 @@ class Root(motionBase.MotionModuleBase):
             cmds.parent(offsetPar, rootCtrl)
             offsetScale = []
             for x in self.ctrlScale:
-                offsetScale.append(x*.75)
+                offsetScale.append(x * 0.75)
             offsetCtrlObj = ctrlCrv.Ctrl(
-                node=offsetCtrl, shape=self.ctrlShapes, scale=offsetScale, orient=[90,0,0]
-                )
+                node=offsetCtrl,
+                shape=self.ctrlShapes,
+                scale=offsetScale,
+                orient=[90, 0, 0],
+            )
             offsetCtrlObj.giveCtrlShape()
 
         cmds.xform(rootPar, ws=True, t=proxyPosition)

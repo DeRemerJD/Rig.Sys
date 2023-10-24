b00ebc2e2b5a9b44218ca5c8fa91f2a7db8ee2c1
gabrieljreed <gabrieljreed@gmail.com>
2023-10-24 11:16:54 -0600

    Remove stuff that doesn't work

--- pytest.bat
+++
@@ -1,3 +0,0 @@
-rem pytest.bat
-@ECHO OFF
-".\mayapy.bat" -m pytest %*
--- rigsys/test/conftest.py
+++
@@ -1,8 +0,0 @@
-import maya.standalone
-import pytest
-
-def pytest_sessionstart(session: pytest.Session):
-    maya.standalone.initialize()
-
-def pytest_sessionfinish(session: pytest.Session, exitstatus: int) -> None:
-    maya.standalone.uninitialize()

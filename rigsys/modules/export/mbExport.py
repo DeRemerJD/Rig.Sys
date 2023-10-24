a122c4a82788faa50447c2ea80523422868d46c1
Gabriel Reed <60671652+gabrieljreed@users.noreply.github.com>
2023-10-23 23:35:51 -0600

    Flesh out mbExport module (#27)

--- rigsys/modules/export/mbExport.py
+++ rigsys/modules/export/mbExport.py
@@ -1,5 +1,8 @@
 """MB Export Module."""
 
+import os
+
+import maya.cmds as cmds
 
 import rigsys.modules.export.exportBase as exportBase
 
@@ -7,11 +10,36 @@ import rigsys.modules.export.exportBase as exportBase
 class MBExport(exportBase.ExportModuleBase):
     """MB Export Module."""
 
-    def __init__(self) -> None:
+    def __init__(self, rig, exportPath: str, label: str = "", buildOrder: int = 5000,
+                 isMuted: bool = False, exportAll: bool = True, exportSelected: bool = False,
+                 nodesToExport: list = None, mirror: bool = False) -> None:
         """Initialize the module."""
-        super().__init__()
+        super().__init__(rig, exportPath, label, buildOrder, isMuted, mirror)
+
+        self.extension = ".mb"
+
+        self.exportAll = exportAll
+        self.exportSelected = exportSelected
+
+        if nodesToExport is None:
+            self.nodesToExport = []
 
     def run(self) -> None:
         """Run the module."""
-        # TODO: Implement
-        pass
+        # Make folders, if necessary
+        exportDir = os.path.dirname(self.fullExportPath)
+        if not os.path.exists(exportDir):
+            os.makedirs(exportDir)
+
+        if self.exportSelected:
+            cmds.select(self.nodesToExport, r=True)
+
+        cmds.file(
+            self.fullExportPath,
+            force=True,
+            options="v=0;",
+            typ="mayaBinary",
+            preserveReferences=True,
+            exportAll=self.exportAll,
+            exportSelected=self.exportSelected,
+        )

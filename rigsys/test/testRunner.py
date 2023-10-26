611a54eaf6333b5ae9d5792da3dc375be76614b4
gabrieljreed <gabrieljreed@gmail.com>
2023-10-26 12:07:55 -0600

    Fix test runner to handle this weird error. https://github.com/pytest-dev/pytest/issues/5462

--- rigsys/test/testRunner.py
+++ rigsys/test/testRunner.py
@@ -1,11 +1,41 @@
 """Run all tests in the test folder."""
 
+import logging
 import os
 import pytest
+import sys
 
 import maya.cmds as cmds
 
 from rigsys.utils.unload import unloadPackages, nukePycFiles
+
+
+logger = logging.getLogger(__name__)
+
+
+class StdOutWrapper:
+    """Wrapper for stdout that fixes pytest capturing."""
+
+    def __init__(self, stdout):
+        """Initialize the wrapper."""
+        self._stdout = stdout
+
+    def __getattr__(self, item):
+        """Get the attribute from the wrapped stdout."""
+        if item == "isatty":
+            return self.isatty
+        else:
+            return getattr(self._stdout, item)
+
+    def isatty(self):
+        """Return False."""
+        return False
+
+
+def fix_stdout():
+    """Fix Maya stdout so that pytest can capture it."""
+    sys.stdout = StdOutWrapper(sys.stdout)
+    sys.stdout._stdout
 
 
 def runTests(pattern="test_"):
@@ -15,7 +45,11 @@ def runTests(pattern="test_"):
 
     testPath = os.path.dirname(__file__)
 
-    pytest.main([testPath, "-k", pattern])
+    try:
+        pytest.main([testPath, "-k", pattern])
+    except Exception:
+        logger.warning("Error running tests. Trying again with fixed stdout.")
+        fix_stdout()
 
     cmds.file(new=True, force=True)
 

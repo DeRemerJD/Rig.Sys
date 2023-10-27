4242a49edec77a7481457d67d34243df9be865a8
Jacob DeRemer <jderemerartist@gmail.com>
2023-10-27 00:20:17 -0400

    Initial Commit

--- rigsys/modules/motion/FKSegment.py
+++ rigsys/modules/motion/FKSegment.py
@@ -1,4 +1,4 @@
-"""World Root Motion Module."""
+"""FK / Reverse FK Motion Module."""
 
 
 import rigsys.modules.motion.motionBase as motionBase
@@ -8,12 +8,12 @@ import rigsys.lib.proxy as proxy
 import maya.cmds as cmds
 
 
-class Root(motionBase.MotionModuleBase):
+class FKSegment(motionBase.MotionModuleBase):
     """Root Motion Module."""
 
-    def __init__(self, rig, side="", label="", ctrlShapes="circle", ctrlScale=None, addOffset=True,
-                 buildOrder: int = 2000, isMuted: bool = False, parent: str = None, mirror: bool = False,
-                 selectedPlug: str = "", selectedParentSocket: str = "") -> None:
+    def __init__(self, rig, side="", label="", ctrlShapes="circle", ctrlScale=None, addOffset=True, segments=1,
+                 reverse=True, buildOrder: int = 2000, isMuted: bool = False, parent: str = None, 
+                 mirror: bool = False, selectedPlug: str = "", selectedParentSocket: str = "") -> None:
         """Initialize the module."""
         super().__init__(rig, side, label, buildOrder, isMuted, parent, mirror, selectedPlug, selectedParentSocket)
 
@@ -23,21 +23,56 @@ class Root(motionBase.MotionModuleBase):
         self.addOffset = addOffset
         self.ctrlShapes = ctrlShapes
         self.ctrlScale = ctrlScale
+        self.segments = segments
+        self.reverse = reverse
 
         self.proxies = {
-            "Root": proxy.Proxy(
+            "Start": proxy.Proxy(
                 position=[0, 0, 0],
                 rotation=[0, 0, 0],
                 side=self.side,
                 label=self.label,
-                name="Base",
+                name="Start"
+            ),
+            "End": proxy.Proxy(
+                position=[0, 10, 0],
+                rotation=[0, 0, 0],
+                side=self.side,
+                label=self.label,
+                name="End",
+                parent="Start"
+            ),
+            "UpVector": proxy.Proxy(
+                position=[0, 0, -10],
+                rotation=[0, 0, 0],
+                side=self.side,
+                label=self.label,
+                name="UpVector",
+                parent="Start"
             )
         }
+        if self.segments > 1:
+            par = "Start"
+            for i in range(1, self.segments):
+                self.proxies[str(i)] = proxy.Proxy(
+                    position=[0, 0, 0],
+                    rotation=[0, 0, 0],
+                    side=self.side,
+                    label=self.label,
+                    name=str(i),
+                    parent=par
+                )
+                par = str(i)
+                if i == self.segments:
+                    self.proxies["End"].parent = str(i)
 
         self.socket = {
-            "Base": None,
-            "Offset": None
+            "Start": None,
+            "End": None
         }
+        if self.segments > 1:
+            for i in range(1, self.segments):
+                self.socket[str(i)] = None
 
     def buildProxies(self):
         """Build the proxies for the module."""
@@ -45,20 +80,30 @@ class Root(motionBase.MotionModuleBase):
 
     def buildModule(self) -> None:
         """Run the module."""
-
-        # Get Proxy pos / rot values
-        proxyPosition = self.proxies["Root"].position
-        proxyRotation = self.proxies["Root"].rotation
-
+        plugPosition = self.proxies["Start"].position
+        plugRotation = self.proxies["Start"].rotation
         # MAKE MODULE NODES
         self.moduleHierarchy()
 
         # Make Plug Transforms
         self.plugParent = self.createPlugParent(
-            position=proxyPosition, rotation=proxyRotation
+            position=plugPosition, rotation=plugRotation
         )
         self.worldParent = self.createWorldParent()
 
+        FKCtrls = []
+        FKGrps = []
+        ### MODULE STRUCTURE ###
+        for key, proxy in self.proxies.items():
+            if key is not "UpVector" or key is not "End":
+                grp = cmds.createNode("transform", n="{}_{}_grp".format(
+                    self.getFullName(), self.proxies[key].name))
+                ctrl = cmds.createNode("transform", n="{}_{}_CTRL".format(
+                    self.getFullName(), self.proxies[key].name))
+                
+                cmds.parent(ctrl, grp)
+                FKCtrls.append(ctrl)
+                FKGrps.append(grp)
         # Structure
         rootPar = cmds.createNode("transform", n=self.getFullName() + "_grp")
         rootCtrl = cmds.createNode("transform", n=self.getFullName() + "_CTRL")
